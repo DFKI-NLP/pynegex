9e8874ef3d79321f7c5de0b0d198f32f2a7a3f1d
Moe binSumait <mh.binsumait@gmail.com>
2023-06-29 13:51:08 +0200

    Update(build): bump build version

--- src/pynegex/__init__.py
+++ src/pynegex/__init__.py
@@ -6,5 +6,5 @@ from pynegex.model.runners import run
 # with open(os.path.join(os.path.dirname(__file__), "VERSION"), "r", encoding="utf-8") as f:
 #     version = f.read().strip()
 
-__version__ = "0.0.1-dev"
+__version__ = "0.0.2-dev"
 # __path__ = __import__('pkgutil').extend_path(__path__, __name__)
